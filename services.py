856348c1c5d1bce9eab4d592b7b1cc0da67dd39b
Rewale <komaroff.sania2015@gmail.com>
2022-01-04 11:46:48 +0500

    add services, add write_video

--- services.py
+++ services.py
@@ -1,5 +1,8 @@
+import shutil
+
 from fastapi import UploadFile
 
 
 def write_video(file_name: str, file: UploadFile):
-    pass
+    with open(file_name, 'wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
