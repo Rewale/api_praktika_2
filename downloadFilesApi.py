5d99aedd7edb12ca07646d906c176ef1d26562fa
Rewale <komaroff.sania2015@gmail.com>
2022-01-04 12:46:31 +0500

    add save_video service

--- downloadFilesApi.py
+++ downloadFilesApi.py
@@ -1,15 +1,15 @@
 import shutil
 from typing import List
-
+from uuid import uuid4
 import ormar
 from fastapi import APIRouter, UploadFile, File, Form, HTTPException
 from starlette.background import BackgroundTasks
 from starlette.requests import Request
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, StreamingResponse
 
 from models import Video, User
 from schemas import UploadVideo, Message, GetVideo
-from services import write_video
+from services import write_video, save_video
 
 video_router = APIRouter()
 
@@ -19,20 +19,22 @@ async def create_video(background_tasks: BackgroundTasks,
                        title: str = Form(...),
                        description: str = Form(...),
                        file: UploadFile = File(...)):
-    file_name = f'media/{file.filename}'
-    if file.content_type == 'video/mp4':
-        background_tasks.add_task(write_video, file_name, file)
-    else:
-        raise HTTPException(status_code=418, detail="It isn't mp4")
-
-    info = UploadVideo(title=title, description=description)
     user = await User.objects.first()
-    return await Video.objects.create(file=file_name, user=user, **info.dict())
+    return await save_video(file=file, description=description, title=title, user_id=user.id,
+                            background_tasks=background_tasks)
 
 
 @video_router.get("/video/{id_video}", response_model=GetVideo, responses={404: {"model": Message}})
 async def get_video(id_video: int):
     try:
-        return await Video.objects.select_related("user").get(id=id_video)
+        file: Video = await Video.objects.select_related("user").get(id=id_video)
+        file_like = open(file.file, mode='rb')
+
+        return StreamingResponse(file_like, media_type='video/mp4')
     except ormar.exceptions.NoMatch:
         return JSONResponse(status_code=404, content=Message(text="No match entity").dict())
+    except FileNotFoundError:
+        return JSONResponse(status_code=404, content=Message(text="Video was deleted").dict())
+
+
+
--- services.py
+++ services.py
@@ -1,6 +1,27 @@
 import shutil
+from uuid import uuid4
 
-from fastapi import UploadFile
+from fastapi import UploadFile, HTTPException
+from starlette.background import BackgroundTasks
+
+from models import Video
+from schemas import UploadVideo
+
+
+async def save_video(file: UploadFile,
+                     user_id: int,
+                     background_tasks: BackgroundTasks,
+                     title: str,
+                     description: str):
+
+    file_name = f'media/{user_id}_{uuid4()}.mp4'
+    if file.content_type == 'video/mp4':
+        background_tasks.add_task(write_video, file_name, file)
+    else:
+        raise HTTPException(status_code=418, detail="It isn't mp4")
+
+    info = UploadVideo(title=title, description=description)
+    return await Video.objects.create(file=file_name, user=user_id, **info.dict())
 
 
 def write_video(file_name: str, file: UploadFile):
